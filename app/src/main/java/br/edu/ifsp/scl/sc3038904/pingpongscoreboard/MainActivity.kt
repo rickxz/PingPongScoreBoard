b9cc41e47cad4ffdc94f3beba34181c763469d0d
rickxz <herick.victor.rodrigues@hotmail.com>
2026-09-01 21:11:40 -0300

    feat(main): exibe a tela da etapa 1 na activity principal

--- app/src/main/java/br/edu/ifsp/scl/sc3038904/pingpongscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038904/pingpongscoreboard/MainActivity.kt
@@ -3,16 +3,19 @@ package br.edu.ifsp.scl.sc3038904.pingpongscoreboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import br.edu.ifsp.scl.sc3038904.pingpongscoreboard.ui.PlacarRemember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,20 +23,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                            .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = stringResource(R.string.titulo_app),
-                            style = MaterialTheme.typography.headlineMedium
-                        )
-                    }
+                    TelaPrincipal(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TelaPrincipal(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.titulo_app),
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        PlacarRemember()
     }
 }
